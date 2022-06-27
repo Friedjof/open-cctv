689dc68704b9e8fe20b7314aa22b0a6cd45fa26b
Friedjof Noweck <friedjof@noweck.info>
2022-06-27 16:57:25 +0200

    add camera stream, database and more

--- app.py
+++ app.py
@@ -1,12 +1,54 @@
-from flask import Flask
+import secrets
+
+from flask import Flask, render_template, Response, request, session, redirect
+
+from modules.camera import VideoCamera
+from modules.model.models import *
 
 app = Flask(__name__)
+app.secret_key = secrets.token_urlsafe(32)
+
+video_stream = VideoCamera()
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return redirect(f'/user/{session["user"]}')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    user: User = User.get_or_none(User.username == f"{request.form['username']}")
+    if user is not None:
+        if user.password == request.form['password']:
+            session['logged_in'] = True
+            session['user'] = request.form['username']
+    return redirect(f'/user/{session["user"]}')
+
+
+@app.route("/user/<name>")
+def home(name):
+    if "user" in tuple(session.keys()):
+        if name == session["user"]:
+            return render_template('index.html', username=name)
+        else:
+            return redirect(f'/user/{session["user"]}')
+    else:
+        return redirect('/')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(video_stream.gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.errorhandler(404)
+def not_found(_):
+    return redirect('/')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='localhost', debug=True, port=5000)
